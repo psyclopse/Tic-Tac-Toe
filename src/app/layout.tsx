f1fbc86e29b9e004d5818536f4742812f8151605
Ola <olamionabanjo0.1@gmail.com>
2026-07-10 22:18:08 +0100

    changed background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
-      <body className="font-sans antialiased text-slate-100">{children}</body>
+      <body className="font-sans antialiased text-stone-900">{children}</body>
     </html>
   );
 }
